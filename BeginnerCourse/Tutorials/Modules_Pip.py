43714367052d74412093b7807293cb913dd1f16f
Raphael Kuchak <rapharbk@gmail.com>
2023-05-30 10:21:41 +0900

    day 6

---
+++ BeginnerCourse/Tutorials/Modules_Pip.py
@@ -0,0 +1,8 @@
+import Modules_Useful_Tools
+import docx
+
+print(Modules_Useful_Tools.roll_dice(10))
+
+#to instal a module with pip
+# go to "Cmd" and type "pip install [module name]" -> exemple: "pip install python-docx"
+# to unistall "pip unistall [module name]"
---
+++ BeginnerCourse/Tutorials/Student_Objects.py
@@ -0,0 +1,8 @@
+from Student_Class import Student
+
+student1 = Student("Rapha", "Programming", 4, False)
+student2 = Student("Ema", "Art", 4, False)
+
+
+print(student1.name)
+print(student2.major)
